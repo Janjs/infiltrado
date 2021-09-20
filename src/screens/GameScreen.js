9f50d40d0f47f5f73c20fcfc518d3d9fbdc95163
Ignasius <i.alonso.1997@gmail.com>
2021-09-20 20:38:05 +0200

    Add Random Number Room

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react"
+
 
 export const GameScreen = () => {
+
+    const [roomCode, setRoomCode] = useState("d");
+
+    useEffect (() => {
+        var randomNum = Math.floor(Math.random() * 9999)
+        setRoomCode(randomNum)
+    }, [])
+
     return (
-        <div>Game Screen</div>
+
+        <div>
+            <div>The code of your room is:</div>
+            <div>{roomCode}</div> 
+
+        </div>
     )
 }
