38d59c055d7de14d0c3dc8a0891204d0bdc71f97
Jan J. Serra <jan.jime.serra@gmail.com>
2021-09-22 13:59:08 +0200

    add generate Room Number

--- src/api/apiFunctions.js
+++ src/api/apiFunctions.js
@@ -1,11 +1,27 @@
 import { db } from "../firebaseConfig";
-import { collection, addDoc, setDoc, doc } from "firebase/firestore";
+import { collection, setDoc, doc, getDoc } from "firebase/firestore";
+
+const checkRoomNumberExists = async (roomNumber) => {
+    const roomSnap = await getDoc(doc(db, "rooms", roomNumber));
+
+    if (roomSnap.exists()) {
+        console.log("Room data:", roomSnap.data());
+    } else {
+        console.log("No such room!");
+    }
+
+    return roomSnap.exists();
+}
 
 export const addRoom = async (username) => {
-    var randomNum = Math.floor(1000 + Math.random() * 9000).toString();
-    console.log(randomNum);
+    do {
+        var randomNum = Math.floor(1000 + Math.random() * 9000).toString();
+        console.log(randomNum);
+    } while (await checkRoomNumberExists(randomNum))
 
     await setDoc(doc(db, "rooms", randomNum), {
         host: username
     });
 }
+
+
