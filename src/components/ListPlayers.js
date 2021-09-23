4923ca3442b0bb1db335ec717947e6b00f208989
Jan J. Serra <jan.jime.serra@gmail.com>
2021-09-23 16:29:35 +0200

    add host name

--- src/components/ListPlayers.js
+++ src/components/ListPlayers.js
@@ -2,12 +2,14 @@ import { useEffect, useState, useContext, useRef } from "react"
 import { db } from "../firebaseConfig";
 import { doc, onSnapshot } from "firebase/firestore";
 import { GameContext } from '../context/GameContext';
+import { Button } from "@mui/material";
 
 export const ListPlayers = (props) => {
     const { state } = useContext(GameContext)
     const { roomNumber } = props
 
     const [players, setPlayers] = useState([state.username])
+    const [isHost, setIsHost] = useState(false)
     const isFirstRender = useRef(true)
 
     const listPlayers = players.map((player) =>
@@ -19,6 +21,9 @@ export const ListPlayers = (props) => {
             onSnapshot(doc(db, "rooms", roomNumber), (doc) => {
                 if (doc.data() !== undefined) {
                     setPlayers(doc.data().players)
+                    if (state.username == doc.data().host) {
+                        setIsHost(true)
+                    }
                 }
             });
         }
@@ -31,6 +36,7 @@ export const ListPlayers = (props) => {
         <div>
             <h3>The code of your room is {roomNumber}</h3>
             <ul>{listPlayers}</ul>
+            {isHost && <Button variant="outlined" onClick={() => console.log("Start button pressed")}>Start Game</Button>}
         </div>
     )
 }
